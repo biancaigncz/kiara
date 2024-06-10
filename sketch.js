21d753453a36bff87e7045c83716673840bd55a3
Bianca Ignacio de Oliveira <00001105637001sp@al.educacao.sp.gov.br>
2024-06-10 20:14:01 -0300

    adicionando o arquivo js css

--- sketch.js
+++ sketch.js
@@ -1,14 +1,15 @@
 
-createCanvas(600, 600);
-background("black")
+function setup() {
+  createCanvas(600, 600);
+  background("black")
 }
 
 function draw() {
-stroke("blue")
-fill ("red")
-
-if (mouseIsPressed){
-  rect(mouseX, mouseY, 20, 35);
-}
-
+  stroke("blue")
+  fill ("red")
+  
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
+ }
+  
 }
