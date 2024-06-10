798acf79cbdcba3028f9bdaed14e23dc52bee11e
Bianca Ignacio de Oliveira <00001105637001sp@al.educacao.sp.gov.br>
2024-06-10 20:23:19 -0300

    alterando as funcoes do arquivo

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 
 function setup() {
   createCanvas(600, 600);
-  background("black")
+  background("black");
 }
 
 function draw() {
-  stroke("blue")
-  fill ("red")
+  stroke("blue");
+  fill ("red");
   
   if (mouseIsPressed){
     rect(mouseX, mouseY, 20, 35);
